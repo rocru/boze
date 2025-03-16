8931f1073a99cd1b7b518dfd02139239178cc414
TwoNick <100712573+TwoNick@users.noreply.github.com>
2025-03-16 14:27:40 -0500

    Render

--- src/main/java/dev/boze/client/render/Framebuffer.java
+++ src/main/java/dev/boze/client/render/Framebuffer.java
@@ -6,32 +6,32 @@ import dev.boze.client.systems.modules.client.Gui;
 import org.lwjgl.opengl.GL32;
 
 public class Framebuffer extends BaseFramebuffer {
-   public int field2150;
+    public int field2150;
 
-   public Framebuffer() {
-      super(false);
-   }
+    public Framebuffer() {
+        super(false);
+    }
 
-   public void initFbo(int width, int height, boolean getError) {
-      RenderSystem.assertOnRenderThreadOrInit();
-      int var7 = RenderSystem.maxSupportedTextureSize();
-      if (width > 0 && width <= var7 && height > 0 && height <= var7) {
-         this.viewportWidth = width;
-         this.viewportHeight = height;
-         this.textureWidth = width;
-         this.textureHeight = height;
-         this.fbo = GlStateManager.glGenFramebuffers();
-         this.colorAttachment = GL32.glGenTextures();
-         GlStateManager._glBindFramebuffer(36160, this.fbo);
-         GL32.glBindTexture(37120, this.colorAttachment);
-         this.field2150 = Gui.INSTANCE.field2362.method461().samples;
-         GL32.glTexImage2DMultisample(37120, this.field2150, 32856, this.textureWidth, this.textureHeight, true);
-         GlStateManager._glFramebufferTexture2D(36160, 36064, 37120, this.colorAttachment, 0);
-         this.checkFramebufferStatus();
-         this.clear(getError);
-         this.endRead();
-      } else {
-         throw new IllegalArgumentException("Window " + width + "x" + height + " size out of bounds (max. size: " + var7 + ")");
-      }
-   }
+    public void initFbo(int width, int height, boolean getError) {
+        RenderSystem.assertOnRenderThreadOrInit();
+        int var7 = RenderSystem.maxSupportedTextureSize();
+        if (width > 0 && width <= var7 && height > 0 && height <= var7) {
+            this.viewportWidth = width;
+            this.viewportHeight = height;
+            this.textureWidth = width;
+            this.textureHeight = height;
+            this.fbo = GlStateManager.glGenFramebuffers();
+            this.colorAttachment = GL32.glGenTextures();
+            GlStateManager._glBindFramebuffer(36160, this.fbo);
+            GL32.glBindTexture(37120, this.colorAttachment);
+            this.field2150 = Gui.INSTANCE.field2362.method461().samples;
+            GL32.glTexImage2DMultisample(37120, this.field2150, 32856, this.textureWidth, this.textureHeight, true);
+            GlStateManager._glFramebufferTexture2D(36160, 36064, 37120, this.colorAttachment, 0);
+            this.checkFramebufferStatus();
+            this.clear(getError);
+            this.endRead();
+        } else {
+            throw new IllegalArgumentException("Window " + width + "x" + height + " size out of bounds (max. size: " + var7 + ")");
+        }
+    }
 }
